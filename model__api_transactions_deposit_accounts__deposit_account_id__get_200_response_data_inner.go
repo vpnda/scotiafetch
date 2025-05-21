9d2045ae5bbc0d5dd4283a77cb2b2944ed5ec61e
vpineda1996 <vpineda@cs.toronto.edu>
2025-05-21 19:34:45 -0400

    Revert "fix: change bool from str"
    This reverts commit aa3c725e9e078753ae84bc38bcf6e354c0b62c0a.

--- model__api_transactions_deposit_accounts__deposit_account_id__get_200_response_data_inner.go
+++ model__api_transactions_deposit_accounts__deposit_account_id__get_200_response_data_inner.go
@@ -29,7 +29,7 @@ type ApiTransactionsDepositAccountsDepositAccountIdGet200ResponseDataInner struc
 	MnemonicCode *string `json:"mnemonicCode,omitempty"`
 	TsysCode NullableString `json:"tsysCode,omitempty"`
 	IsTsys *bool `json:"isTsys,omitempty"`
-	IsDisputable *bool `json:"isDisputable,omitempty"`
+	IsDisputable NullableBool `json:"isDisputable,omitempty"`
 	OriginalAmount *ApiTransactionsDepositAccountsDepositAccountIdGet200ResponseDataInnerOriginalAmount `json:"originalAmount,omitempty"`
 	RunningBalance *ApiAccountsSummaryGet200ResponseDataProductsInnerPrimaryBalancesInner `json:"runningBalance,omitempty"`
 	AssociatedCardNumber NullableString `json:"associatedCardNumber,omitempty"`
@@ -434,11 +434,11 @@ func (o *ApiTransactionsDepositAccountsDepositAccountIdGet200ResponseDataInner) 
 
 // GetIsDisputable returns the IsDisputable field value if set, zero value otherwise (both if not set or set to explicit null).
 func (o *ApiTransactionsDepositAccountsDepositAccountIdGet200ResponseDataInner) GetIsDisputable() bool {
-	if o == nil || IsNil(o.IsDisputable) {
+	if o == nil || IsNil(o.IsDisputable.Get()) {
 		var ret bool
 		return ret
 	}
-	return *o.IsDisputable
+	return *o.IsDisputable.Get()
 }
 
 // GetIsDisputableOk returns a tuple with the IsDisputable field value if set, nil otherwise
@@ -448,13 +448,13 @@ func (o *ApiTransactionsDepositAccountsDepositAccountIdGet200ResponseDataInner) 
 	if o == nil {
 		return nil, false
 	}
-	return o.IsDisputable, o.IsDisputable != nil
+	return o.IsDisputable.Get(), o.IsDisputable.IsSet()
 }
 
 // HasIsDisputable returns a boolean if a field has been set.
 func (o *ApiTransactionsDepositAccountsDepositAccountIdGet200ResponseDataInner) HasIsDisputable() bool {
-	if o != nil && o.IsDisputable != nil {
-		return *o.IsDisputable
+	if o != nil && o.IsDisputable.IsSet() {
+		return true
 	}
 
 	return false
@@ -462,16 +462,16 @@ func (o *ApiTransactionsDepositAccountsDepositAccountIdGet200ResponseDataInner) 
 
 // SetIsDisputable gets a reference to the given NullableBool and assigns it to the IsDisputable field.
 func (o *ApiTransactionsDepositAccountsDepositAccountIdGet200ResponseDataInner) SetIsDisputable(v bool) {
-	o.IsDisputable = &v
+	o.IsDisputable.Set(&v)
 }
 // SetIsDisputableNil sets the value for IsDisputable to be an explicit nil
 func (o *ApiTransactionsDepositAccountsDepositAccountIdGet200ResponseDataInner) SetIsDisputableNil() {
-	o.IsDisputable = nil
+	o.IsDisputable.Set(nil)
 }
 
 // UnsetIsDisputable ensures that no value is present for IsDisputable, not even an explicit nil
 func (o *ApiTransactionsDepositAccountsDepositAccountIdGet200ResponseDataInner) UnsetIsDisputable() {
-	o.IsDisputable = nil
+	o.IsDisputable.Unset()
 }
 
 // GetOriginalAmount returns the OriginalAmount field value if set, zero value otherwise.
@@ -1720,8 +1720,8 @@ func (o ApiTransactionsDepositAccountsDepositAccountIdGet200ResponseDataInner) T
 	if !IsNil(o.IsTsys) {
 		toSerialize["isTsys"] = o.IsTsys
 	}
-	if !IsNil(o.IsDisputable) {
-		toSerialize["isDisputable"] = o.IsDisputable
+	if o.IsDisputable.IsSet() {
+		toSerialize["isDisputable"] = o.IsDisputable.Get()
 	}
 	if !IsNil(o.OriginalAmount) {
 		toSerialize["originalAmount"] = o.OriginalAmount
